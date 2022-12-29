28d55ca00ed9b595525c70d260ed7deeb674f301
paruljamwal <paruljamwal2000@gmail.com>
2022-12-29 17:30:18 +0530

    submission :)

--- Mapmakers/src/App.js
+++ Mapmakers/src/App.js
@@ -1,12 +1,70 @@
+import "./App.css";
+import {
+  Map,
+  InfoWindow,
+  Marker,
+  GoogleApiWrapper,
+  Polyline,
+} from "google-maps-react";
+import { Component } from "react";
 
-import './App.css';
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      locations: [],
+      initLoc: {},
+    };
+  }
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  componentDidMount() {
+    fetch(`https://comfortable-newt-shift.cyclic.app/map`)
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({
+          locations: json.randomVal,
+          initLoc: json.randomVal[0],
+        });
+      });
+  }
+
+  render() {
+    return (
+      <>
+        <Map
+          google={this.props.google}
+          zoom={3}
+          center={{
+            lat: this?.state?.initLoc?.lat,
+            lng: this?.state?.initLoc?.lng,
+          }}
+          onClick={this.onMapClicked}
+        >
+          <Polyline
+            path={this?.state?.locations}
+            strokeColor="red"
+            strokeOpacity={0.8}
+            strokeWeight={4}
+          />
+
+          <Marker name={"Current location"} />
+          <InfoWindow onClose={this.onInfoWindowClose}>
+            <div>
+              <h1>Home</h1>
+            </div>
+          </InfoWindow>
+          {this?.state?.locations?.map((location) => (
+            <Marker
+              title={"marker"}
+              position={{ lat: location.lat, lng: location.lng }}
+            />
+          ))}
+        </Map>
+      </>
+    );
+  }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyCqFay6qqmew8KKgujKBlhSNzA7RAr4yqQ",
+})(App);
